9151742cae8d8767f81c860318dba657d34542a8
KEEPlagat <21781968-KEEPlagat@users.noreply.replit.com>
2023-04-13 07:58:20 +0000

    Added routing and dynamic pages

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'title': "Data analyst",
+  'location': "kenya",
+  'salary': "Ksh 3200"
+}, {
+  'id': 2,
+  'title': "Data analyst",
+  'location': "kenya",
+  'salary': "Ksh 3200"
+}, {
+  'id': 3,
+  'title': "Data analyst",
+  'location': "kenya",
+  'salary': "Ksh 3200"
+}]
 
 
 @app.route('/')
 def index():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 
 app.run(host='0.0.0.0', port=81, debug=True)
