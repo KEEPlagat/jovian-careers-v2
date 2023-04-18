a49715ed21ca05c6130e785be1ec919db6bcf45d
KEEPlagat <104130403+KEEPlagat@users.noreply.github.com>
2023-04-18 17:04:00 +0000

    Added connection string

---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine, text
+
+DATABASE = 'joviancareers'
+USER = 'eolyi4s9kr15eryqb7im'
+PASSWORD = 'pscale_pw_CkdJKS61wddkMcTnhdairHJZdCTOyVkLPUVQYInSdPA'
+HOST = 'aws.connect.psdb.cloud'
+PORT = 3306  # default port for MySQL
+
+DATABASE_URL = f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
+
+ssl_args = {"ssl": {"ssl_ca": "/etc/ssl/cert.pem"}}
+# create the engine
+engine = create_engine(DATABASE_URL, connect_args=ssl_args)
+
+try:
+  with engine.connect() as conn:
+    print("Connected successfully!")
+except Exception as ex:
+  print(f"Connection failed: {str(ex)}")
